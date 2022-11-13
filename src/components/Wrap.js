ecaf8b2955b33d8f128fd9f5d5c9ad681002d4e8
김태완 <tay.tw.kim@gmail.com>
2022-11-12 23:49:41 -0500

    allowance check

--- src/components/Wrap.js
+++ src/components/Wrap.js
@@ -1,6 +1,8 @@
 //The component and functions on this file are from: 
 //https://docs.superfluid.finance/superfluid/developers/constant-flow-agreement-cfa/money-streaming-1
+
 //adjusted to connect to web3 provider (metamask)
+//adjusted to request approval only if the amount is greater than the current allowance on contract
 
 import React, { useState } from "react";
 import { customHttpProvider } from "../config";
@@ -9,6 +11,32 @@ import { ethers } from "ethers";
 import { daiABI } from "../config";
 import { Button, Form, FormGroup, FormControl, Spinner } from "react-bootstrap";
 import "../css/wrap.css";
+
+async function getAllowance(){
+  const sf = await Framework.create({
+    chainId: 5,
+    provider: customHttpProvider
+  });
+
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  
+  const fDAI = new ethers.Contract(
+    "0x88271d333C72e51516B67f5567c728E702b3eeE8",
+    daiABI,
+    signer
+  );
+
+  try{
+    await fDAI.allowance(
+      "0x643a5a4e68C90A72642326E34FC1A181a3f5F8c4",
+      "0xF2d68898557cCb2Cf4C10c3Ef2B034b2a69DAD00",
+    ).then((value)=>{console.log(value.toString());});
+  } catch (error){
+    console.log(error);
+  }
+  
+}
 
 //will be used to approve super token contract to spend DAI
 async function daiApprove(amt) {
@@ -22,19 +50,19 @@ async function daiApprove(amt) {
 
   //fDAI on goerli: you can find network addresses here: https://docs.superfluid.finance/superfluid/developers/networks
   //note that this abi is the one found here: https://goerli.etherscan.io/address/0x88271d333C72e51516B67f5567c728E702b3eeE8
-  const DAI = new ethers.Contract(
+  const fDAI = new ethers.Contract(
     "0x88271d333C72e51516B67f5567c728E702b3eeE8",
     daiABI,
     signer
   );
   try {
     console.log("approving DAI spend");
-    await DAI.approve(
+    await fDAI.approve(
       "0xF2d68898557cCb2Cf4C10c3Ef2B034b2a69DAD00",
       ethers.utils.parseEther(amt.toString())
     ).then(function (tx) {
       console.log(
-        `Congrats, you just approved your DAI spend. You can see this tx at https://kovan.etherscan.io/tx/${tx.hash}`
+        `Congrats, you just approved your DAI spend. You can see this tx at https://goerli.etherscan.io/tx/${tx.hash}`
       );
     });
   } catch (error) {
@@ -102,6 +130,13 @@ export const Wrap = () => {
   return (
     <div className="container">
       <h2>Wrap Token</h2>
+      <Button onClick={() => {
+              getAllowance();
+              setTimeout(() => {
+                setIsApproveButtonLoading(false);
+              }, 1000);
+            }}>allowance      
+      </Button>
       <Form>
         <FormGroup className="mb-3">
           <FormControl
